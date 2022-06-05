ae4b7ce541b9c704cd42ee071a887d81c8ceea81
Adrian Cvijanović <adrian.cvijanovic@gmail.com>
2022-06-06 00:05:25 +0200

    this commit exists because xcode sucks

---
+++ GameDeals/GameDeals/Presentation/ViewExtensions/UINavigationController/UINavigationControllerExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UINavigationControllerExtension.swift
+//  GameDeals
+//
+//  Created by Five on 05.06.2022..
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController {
+    static func createAppNavigationController() -> UINavigationController {
+        let navigationController = UINavigationController()
+        navigationController.navigationBar.tintColor = .white
+        navigationController.navigationBar.backgroundColor = .green
+        navigationController.navigationBar.isTranslucent = true
+        
+        
+        return navigationController
+    }
+}
